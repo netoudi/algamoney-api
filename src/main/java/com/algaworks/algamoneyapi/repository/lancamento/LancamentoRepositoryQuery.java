36317f20459ae34e7dc2eafe85847e8bae6d016d
netoudi <netoudi@rocketmail.com>
2017-07-28 12:25:59 -0300

    Implementando a paginação de lançamentos

--- src/main/java/com/algaworks/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/algaworks/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,12 +1,13 @@
 package com.algaworks.algamoneyapi.repository.lancamento;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.algaworks.algamoneyapi.model.Lancamento;
 import com.algaworks.algamoneyapi.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 
-	List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 
 }
